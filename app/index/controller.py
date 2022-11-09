4ae907572963e0410f8d341f7706936d59b5be7b
Fabio Davilla <fabiomgdavilla@gmail.com>
2022-11-09 17:27:50 -0300

    almost finished

--- app/index/controller.py
+++ app/index/controller.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, render_template
+from app.models.answer import Answer
+from app.models.quiz import Quiz
+
+from app.models.user import User
 
 index_controller = Blueprint('index', __name__, url_prefix='/')
 
 
 @index_controller.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    users = User.query.count()
+    quizzes_active = Quiz.query.filter_by(active=True).count()
+    quizzes_inactive = Quiz.query.filter_by(active=False).count()
+    answers = Answer.query.count()
+
+    return render_template("index.html", data=[users, quizzes_active + quizzes_inactive, quizzes_active, quizzes_inactive, answers])
